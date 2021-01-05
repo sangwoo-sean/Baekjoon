ba4b4e7a173353914e2968ba5178017234ab8fa0
Sangwoo Park <71590696+SW-Park-kor@users.noreply.github.com>
2021-01-06 00:51:03 +0900

    Add files via upload

---
+++ 10773(제로).py
@@ -0,0 +1,32 @@
+#느려서 sys input 받으니 엄청빨라짐
+
+
+#deque를 이용한 솔루션 (더 느림)
+# from collections import deque
+
+# n = int(input())
+# result = deque([])
+# for i in range(n):
+#     a = int(input())
+
+#     if a == 0:
+#         result.pop()
+#     else:
+#         result.append(a)
+
+# print(sum(result))
+
+
+
+# 내 첫 솔루션
+# n = int(input())
+# for i in range(n):
+#     a = int(input())
+
+#     if a == 0:
+#         result.pop()
+#     else:
+#         result.append(a)
+
+
+# print(sum(result))
