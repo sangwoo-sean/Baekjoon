1d8d06f51eac39862140b75a3f6740c2d7d398a9
SangwooPark-Sean <tkddn3367@gmail.com>
2021-02-10 17:49:36 +0900

    update condition when n==1

--- 9465(스티커)(DP).py
+++ 9465(스티커)(DP).py
@@ -8,12 +8,14 @@ for _ in range(int(input())):
     dp = [[0 for _ in range(n)], [0 for _ in range(n)]]
     dp[0][0] = stk[0][0]
     dp[1][0] = stk[1][0]
-    dp[0][1] = stk[0][1] + dp[1][0]
-    dp[1][1] = stk[1][1] + dp[0][0]
+    if n > 1:
+        dp[0][1] = stk[0][1] + dp[1][0]
+        dp[1][1] = stk[1][1] + dp[0][0]
+        for i in range(2, n):
+            dp[0][i] = stk[0][i] + max(dp[1][i-1], dp[1][i-2])
+            dp[1][i] = stk[1][i] + max(dp[0][i-1], dp[0][i-2])
 
-    for i in range(2, n):
-        dp[0][i] = stk[0][i] + max(dp[1][i-1], dp[1][i-2])
-        dp[1][i] = stk[1][i] + max(dp[0][i-1], dp[0][i-2])
-
-    ans = max(dp[0][-1], dp[0][-2], dp[1][-1], dp[1][-2])
+        ans = max(dp[0][-1], dp[0][-2], dp[1][-1], dp[1][-2])
+    else:
+        ans = max(stk[0][0], stk[1][0])
     print(ans)
