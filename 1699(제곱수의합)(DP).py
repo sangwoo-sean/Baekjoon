9396dc7650167128f9948d29ec3eb2549680ba8d
Sangwoo Park <tkddn3367@gmail.com>
2021-03-14 00:30:30 +0900

    21.03.14

--- 1699(제곱수의합)(DP).py
+++ 1699(제곱수의합)(DP).py
@@ -13,3 +13,33 @@ for i in range(1, n+1):
     table[i] = x + 1
 
 print(table[n])
+
+# #다른풀이
+# def solve(n):
+#     if int((n**0.5))**2 == n:
+#         return 1
+
+#     for s in sqs:
+#         if n-s < 0:
+#             break
+#         x = int((n-s)**0.5)**2
+#         if x == n-s:
+#             return 2
+
+#     for s in sqs:
+#         if n-s < 0:
+#             break
+#         for t in sqs:
+#             if n-s-t < 0:
+#                 break
+#             if int((n-s-t)**0.5)**2 == n-s-t:
+#                 return 3
+
+#     return 4
+
+
+# # for n in range(1, int(input())+1):
+# n = int(input())
+
+# sqs = [i**2 for i in range(1, int(n**0.5)+1)]
+# print(solve(n))
