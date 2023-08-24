8351e7848ccdd4fd1b6db6d94a97bab9a32c1a91
sangwoo <tkddn3367@gmail.com>
2023-08-24 19:14:03 +0900

    2439

---
+++ 2439.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+# for i in range(n):
+#     for j in range(n - i - 1):
+#         print(" ", end="")
+#     for j in range(i + 1):
+#         print("*", end="")
+#     print()
+
+
+for i in range(1, n + 1):
+    print(" " * (n - i) + "*" * i)
